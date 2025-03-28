9ae6e8954d9fc7f0a2435413af9785db08c049e6
Mattdias312 <112134994+Mattdias312@users.noreply.github.com>
2025-03-28 13:36:00 -0300

    feat: atualiza testes

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,31 +1,22 @@
-// tests/index.test.js
 const request = require('supertest');
-const app = require('../src/index');
-
-describe('GET /api', () => {
-  it('should return Hello world!', async () => {
-    const res = await request(app).get('/api');
-    expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello world!');
-  });
-});
+const app = require('../src/JS/index'); // Ajuste o caminho
 
 describe('GET /api/test', () => {
-  it('should return a test message', async () => {
+  it('should return test message with status', async () => {
     const res = await request(app).get('/api/test');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Endpoint de teste');
+    expect(res.body).toEqual({
+      message: 'Endpoint de teste consolidado',
+      status: 'success',
+      code: 200
+    });
   });
 });
 
-/*
-describe('GET /api/test', () => {
-  it('should return a test message', async () => {
+describe('GET /api/test-2', () => {
+  it('should return test message 2', async () => {
     const res = await request(app).get('/api/test-2');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('message', 'Endpoint de teste 2');
+    expect(res.body).toHaveProperty('message', 'Branch master endpoint teste 2');
   });
 });
-*/
-
-jest.setTimeout(30000); 
